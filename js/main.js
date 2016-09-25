58f5a4c582bd82e075d670f121413370c2887806
Paul Bevis <paulbevis2010@gmail.com>
2016-09-25 18:04:08 +1000

    svn animation code

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+
+  var timeLineMax = new TimelineMax({yoyo: true, repeat: -1});
+  var cubes = $('.cubes'), cubeA = $('.a'), cubeB = $('.b'), cubeC = $('.c'), cubeD = $('.d'),
+    cubeE = $('.e'), cubeF = $('.f'), cubeG = $('.g'), cubeH = $('.h'), cubeI = $('.i');
+
+  timeLineMax
+    .set(spinner, {scale: 0.25, autoAlpha: 1})
+    .staggerFrom([cubeA, cubeB, cubeC, cubeD, cubeE, cubeF, cubeG, cubeH, cubeI], .6, {fill: 'green', autoAlpha: 0}, 0.3)
+
+})(jQuery);
+
+
